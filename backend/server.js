e0f6781c1184439b9db23563229cf54b2a437c96
oliveszn <ekpealamicheal@gmail.com>
2024-05-21 05:39:44 +0100

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
+// const userRoutes = require('./routes/user')
+
+// express app
+const app = express();
+
+// middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use("/api/workouts", workoutRoutes);
+
+// app.listen(process.env.PORT, () => {
+//   console.log("liste");
+// });
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
