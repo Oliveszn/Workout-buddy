1ac1be8484180d7f23b60fb4555d9fe92aa9f0b3
oliveszn <ekpealamicheal@gmail.com>
2024-05-22 13:24:54 +0100

    react setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
