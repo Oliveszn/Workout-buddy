148dd274d3825b93d981231cbcef1f1d88afd404
oliveszn <ekpealamicheal@gmail.com>
2024-05-25 23:47:50 +0100

    added form

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1>Workout Buddy</h1>
+        </Link>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
