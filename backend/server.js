254d4c0c40a742b489e68b4ee9b60d305c1412ff
oliveszn <ekpealamicheal@gmail.com>
2024-06-06 11:07:22 +0100

    plenty

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-const express = require("express");
 require("dotenv").config();
 
+const express = require("express");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/user");
